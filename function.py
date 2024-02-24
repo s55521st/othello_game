71c61f07e35bbda94e2420c9324cfa2fd00172da
tsunemi <67031970+s55521st@users.noreply.github.com>
2024-02-24 13:30:17 +0900

    Update function.py

--- function.py
+++ function.py
@@ -1,23 +1,30 @@
 # 有効な手かどうかをチェック
 def is_valid_move(board, row, col, player):
+    # すでに駒が置かれている場合は無効な移動
     if board[row][col] != 0:
         return False
 
-    directions = [
-        (0, 1), (0, -1), (1, 0), (-1, 0),
-        (1, 1), (-1, -1), (-1, 1), (1, -1)
-    ]
+    # 8方向の移動をチェック
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]
+    valid_move = False
 
     for dr, dc in directions:
         r, c = row + dr, col + dc
-        if 0 <= r < 8 and 0 <= c < 8 and board[r][c] != player and board[r][c] != 0:
-            while 0 <= r < 8 and 0 <= c < 8 and board[r][c] != 0:
-                r += dr
-                c += dc
-                if 0 <= r < 8 and 0 <= c < 8 and board[r][c] == player:
-                    return True
+        found_opponent = False
 
-    return False
+        while 0 <= r < 8 and 0 <= c < 8:
+            if board[r][c] == 0:
+                break
+            elif found_opponent == False and board[r][c] == player:
+                break
+            elif board[r][c] != player:
+                found_opponent = True
+            elif board[r][c] == player and found_opponent:
+                valid_move = True
+                break
+            r, c = r + dr, c + dc
+
+    return valid_move
 
 # 石を置く処理
 def make_move(board, row, col, player):
